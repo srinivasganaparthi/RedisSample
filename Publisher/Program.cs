cfc8e49ba7d2aa8a31f6496ca430a842ee872b25
Srinivas Ganaparthi <sganaparthi@ivycomptech.com>
2022-07-10 10:25:30 +0530

    initial commit

---
+++ Publisher/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StackExchange.Redis;
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(
+        new ConfigurationOptions
+        {
+            EndPoints = { "127.0.0.1:6379" }
+        });
+var db = redis.GetDatabase();
+var pong = await db.PingAsync();
+Console.WriteLine(pong);
+
+
+ISubscriber sub = redis.GetSubscriber();
+PublishMessages();
+
+void PublishMessages()
+{
+    Console.WriteLine("Enter Number");
+    var message = Console.ReadLine();
+
+    sub.Publish("messages", message);
+
+    PublishMessages();
+}
+
+
---
+++ Subscriber/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StackExchange.Redis;
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(
+        new ConfigurationOptions
+        {
+            EndPoints = { "127.0.0.1:6379" }
+        });
+var db = redis.GetDatabase();
+var pong = await db.PingAsync();
+Console.WriteLine(pong);
+
+
+ISubscriber sub = redis.GetSubscriber();
+
+sub.Subscribe("messages").OnMessage(async channelMessage => {
+    await Task.Delay(1000);
+    int value = Convert.ToInt32(channelMessage.Message);
+    if (value % 2 == 0) 
+       Console.WriteLine(Convert.ToString(channelMessage.Message));
+});
+
+Console.ReadLine();
